32c891416cde5831e4ec59484c16549f88ae99cf
mdjaforali2 <mdjaforali2@gmail..com>
2023-10-21 16:45:17 -0400

    first commit

--- dennisivy/settings.py
+++ dennisivy/settings.py
@@ -149,7 +149,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = "mdjaforali2@gmail.com"
-EMAIL_HOST_PASSWORD = 'bnasuiyktjslvgsa'
+EMAIL_HOST_PASSWORD = 'llpmpfgrlklmtyxj'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
